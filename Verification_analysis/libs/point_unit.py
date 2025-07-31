dd714c944723f2d39b4138d3cf43ddf35625cc36
nikitinas <as.nikitin@promenergo-rt.ru>
2025-07-31 14:03:33 +0300

    удалил

--- Verification_analysis/libs/point_unit.py
+++
@@ -1,6 +0,0 @@
-class PointUnit:
-    def __init__(self, column, minimum, maximum):
-        self.value = None
-        self.column = column
-        self.minimum = minimum
-        self.maximum = maximum
